8cc6f705e759d7c5fcf1eddcb7c75ab71fd0f877
bharat1226 <bharatgoli92@gmail.com>
2016-04-20 14:50:11 -0700

    Completed First Job

--- src/FirstMapreducerClass.java
+++ src/FirstMapreducerClass.java
@@ -1,4 +1,77 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import java.io.IOException;
+import java.util.*;
+
 
 public class FirstMapreducerClass {
+        public static class FirstMapper extends Mapper<LongWritable, Text, Text, Text> {
+            public int counter = 0;
+            public void map(LongWritable key, Text value, Context context){
+                    context.getInputSplit();
+                    String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+                    String line = value.toString();
+                    String empty = "";
+                    String[] str = new String[2];
+                    if(counter > 0) {
+                        try {
+                            String[] temp = line.split(",");
+                            str = temp[0].split("-");
+                            empty = str[2] + " " + temp[6];
+                        }
+                        catch (Exception e)
+                        {
+                            System.out.println(line + counter);
+                        }
+                    }
+                counter++;
+                try {
+                    if(counter > 1)
+                        context.write(new Text(fileName.split("\\.")[0] + "-" + str[0] + "-" + str[1]),new Text(empty));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
+
+    /**
+     * input: <key, value>, key = Text, value: number of occurrence
+     * output: <key, value>, key = Text, value = number of occurrence
+     *
+     * */
+
+    public static class FirstReducer extends Reducer<Text, Text, Text, Text> {
+
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Iterator<Text> iter = values.iterator();
+            HashMap<Integer, Double> dayValue = new HashMap<Integer, Double>();
+
+            while(iter.hasNext()){
+                String s = iter.next().toString();
+                dayValue.put(Integer.parseInt(s.split(" ")[0]), Double.parseDouble(s.split(" ")[1]));
+            }
+
+            TreeMap<Integer, Double> treeMap = new TreeMap<Integer, Double>(dayValue);
+
+            java.util.Map.Entry<Integer, Double> first = treeMap.firstEntry();
+            java.util.Map.Entry<Integer, Double> last = treeMap.lastEntry();
+
+            double rateOfReturn = (last.getValue() - first.getValue())/first.getValue();
+
+            Text value = new Text(String.valueOf(rateOfReturn));
+            try {
+                context.write(key, value);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Job firstJob = Job.getInstance();
+        
+        firstJob.setJarByClass(FirstMapreducerClass.class);
+        
+        firstJob.setMapperClass(FirstMapreducerClass.FirstMapper.class);
+        firstJob.setReducerClass(FirstMapreducerClass.FirstReducer.class);
 
+        firstJob.setMapOutputKeyClass(Text.class);
+        firstJob.setMapOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(firstJob, new Path(args[0]));
+        FileOutputFormat.setOutputPath(firstJob, new Path("First_Job_output"+args[1]));
+
+        firstJob.waitForCompletion(true);
+    }
 }

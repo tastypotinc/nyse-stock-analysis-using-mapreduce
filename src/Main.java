bcc603126a7b8b294dc764b796b716ecc3fec4d0
bharat1226 <bharatgoli92@gmail.com>
2016-04-20 15:16:31 -0700

    Completed third job

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,48 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         Job firstJob = Job.getInstance();
         Job secondJob = Job.getInstance();
-        
+        Job thirdJob = Job.getInstance();
+
         firstJob.setJarByClass(FirstMapreducerClass.class);
         secondJob.setJarByClass(SecondMapreducerClass.class);
-        
+        thirdJob.setJarByClass(ThirdMapreducerClass.class);
+
         firstJob.setMapperClass(FirstMapreducerClass.FirstMapper.class);
         firstJob.setReducerClass(FirstMapreducerClass.FirstReducer.class);
-        
+
         secondJob.setMapperClass(SecondMapreducerClass.SecondMapper.class);
         secondJob.setReducerClass(SecondMapreducerClass.SecondReducer.class);
 
+        thirdJob.setMapperClass(ThirdMapreducerClass.ThirdMapper.class);
+        thirdJob.setReducerClass(ThirdMapreducerClass.ThirdReducer.class);
+
         firstJob.setMapOutputKeyClass(Text.class);
         firstJob.setMapOutputValueClass(Text.class);
-        
+
         secondJob.setMapOutputKeyClass(Text.class);
         secondJob.setMapOutputValueClass(Text.class);
 
+        thirdJob.setMapOutputKeyClass(Text.class);
+        thirdJob.setMapOutputValueClass(Text.class);
+
+
         FileInputFormat.addInputPath(firstJob, new Path(args[0]));
-        FileOutputFormat.setOutputPath(firstJob, new Path("First_Job_output"+args[1]));
-        
+        FileOutputFormat.setOutputPath(firstJob, new Path("Intermediate_Data_1_"+args[1]));
+
         FileInputFormat.addInputPath(secondJob, new Path("Intermediate_Data_1_"+args[1]));
         FileOutputFormat.setOutputPath(secondJob, new Path("Intermediate_Data_2_"+args[1]));
-        
-        secondJob.waitForCompletion(true);
+
+        FileInputFormat.addInputPath(thirdJob, new Path("Intermediate_Data_2_"+args[1]));
+        FileOutputFormat.setOutputPath(thirdJob, new Path("Final_Data_"+args[1]));
 
         firstJob.waitForCompletion(true);
+        secondJob.waitForCompletion(true);
+        thirdJob.waitForCompletion(true);
+
+
     }
 }
--- src/ThirdMapreducerClass.java
+++ src/ThirdMapreducerClass.java
@@ -1,4 +1,62 @@
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ThirdMapreducerClass {
 
+    public static class ThirdMapper extends Mapper<LongWritable, Text, Text, Text> {
+        public void map(LongWritable key, Text value, Context context) {
+            String line = value.toString();
+            String newKey = line.toString().split("\t")[0];
+
+            try {
+                context.write(new Text("unUsed"), new Text(newKey));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static class ThirdReducer extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        List<String> list = new ArrayList<String>();
+            for(Text val : values){
+                list.add(val.toString());
+            }
+
+            Collections.sort(list);
+
+            try {
+
+                context.write(new Text("Top 5 lowest volatility companies"), new Text(""));
+                context.write(new Text("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"), new Text(""));
+                for(int i = 0; i < 5; i++){
+                    context.write(new Text(list.get(i).split(" ")[1]), new Text(list.get(i).split(" ")[0]));
+                }
+
+                context.write(new Text("\nTop 10 highest volatility companies"), new Text(""));
+                context.write(new Text("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"), new Text(""));
+
+                for(int i = list.size() - 1; i >= list.size() - 10; i--){
+                    context.write(new Text(list.get(i).split(" ")[1]), new Text(list.get(i).split(" ")[0]));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
+
+

2e5e83a9e2b87816d548dbe3f09c9d072c12ee65
bharat1226 <bharatgoli92@gmail.com>
2016-04-20 14:56:07 -0700

    Completed Second Job

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,30 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
     public static void main(String[] args) throws Exception {
         Job firstJob = Job.getInstance();
+        Job secondJob = Job.getInstance();
         
         firstJob.setJarByClass(FirstMapreducerClass.class);
+        secondJob.setJarByClass(SecondMapreducerClass.class);
         
         firstJob.setMapperClass(FirstMapreducerClass.FirstMapper.class);
         firstJob.setReducerClass(FirstMapreducerClass.FirstReducer.class);
+        
+        secondJob.setMapperClass(SecondMapreducerClass.SecondMapper.class);
+        secondJob.setReducerClass(SecondMapreducerClass.SecondReducer.class);
 
         firstJob.setMapOutputKeyClass(Text.class);
         firstJob.setMapOutputValueClass(Text.class);
+        
+        secondJob.setMapOutputKeyClass(Text.class);
+        secondJob.setMapOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(firstJob, new Path(args[0]));
         FileOutputFormat.setOutputPath(firstJob, new Path("First_Job_output"+args[1]));
+        
+        FileInputFormat.addInputPath(secondJob, new Path("Intermediate_Data_1_"+args[1]));
+        FileOutputFormat.setOutputPath(secondJob, new Path("Intermediate_Data_2_"+args[1]));
+        
+        secondJob.waitForCompletion(true);
 
         firstJob.waitForCompletion(true);
     }
--- src/SecondMapreducerClass.java
+++ src/SecondMapreducerClass.java
@@ -1,4 +1,65 @@
 
-public class SecondMapreducerClass {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class SecondMapreducerClass {
+    public static class SecondMapper extends Mapper<LongWritable, Text, Text, Text> {
+        public void map(LongWritable key, Text value, Context context){
+            String line = value.toString();
+            String newKey = line.toString().split("\t")[0].split("-")[0].split(" ")[0];
+            String val = line.toString().split("\t")[1];
+            try {
+                context.write(new Text(newKey),new Text(val));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * input: <key, value>, key = Text, value: number of occurrence
+     * output: <key, value>, key = Text, value = number of occurrence
+     * */
+
+    public static class SecondReducer extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            List<Double> list = new ArrayList<Double>();
+            for (Text val : values) {
+                list.add(Double.parseDouble(val.toString()));
+            }
+            Double rateOfReturnSum = 0.0;
+            int i=0;
+
+            for (Double doub:list) {
+            	rateOfReturnSum += doub;
+                i++;
+            }
+
+            Double avg = rateOfReturnSum/i;
+            Double mean_sqr_diff = 0.0;
+
+            for (Double d:list) {
+                mean_sqr_diff = mean_sqr_diff + Math.pow((d - avg), 2);
+            }
+
+            double volatility = Math.sqrt(mean_sqr_diff/i);
+            String temp = Double.toString(volatility);
+
+            try {
+                context.write(new Text(temp + " " + key), new Text(temp));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
